73ff84f76d4fe083dec1b497e334e920cac7e085
rxliuli <rxliuli@gmail.com>
2019-04-09 20:56:47 +0800

    Initial commit

---
+++ src/main/java/com/rxliuli/mavenrepositoryexample/MavenStart.java
@@ -0,0 +1,10 @@
+package com.rxliuli.mavenrepositoryexample;
+
+/**
+ * @author rxliuli
+ */
+public class MavenStart {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
